be80b31a5dd0360781334b316d4a7957aef2c9dc
DevNeoLee <devneolee@gmail.com>
2021-10-27 13:24:42 -0700

    Add webhook_controller and graphql

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
   resources :brokerage_jobs
   root 'brokerage_jobs#index'
   
+  constraints subdomain: "hooks" do
+    post '/:webhook_name' => 'webhooks#receive', as: :receive_webhooks
+  end
+
+  # if Rails.env.development?
+  #   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
+  # end
+
+  post "/graphql", to: "graphql#execute"
+
 end
